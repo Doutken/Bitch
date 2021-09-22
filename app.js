73164ddf348c28afcf4472acece8bb32e64aee82
Ruben Paredes <ruben.pardes25@gmail.com>
2021-09-22 16:52:13 -0400

    first button

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
 console.log("Hello World");
+let counter = 5000;
+function startTimer() {
+    setTimeout(() => {
+    alert("You're done!")
+    }, 5000);
+};
+
+function backwardsCount() {
+    setInterval(() => {
+counter-- 
+console.log(counter)
+}, 1000)
+};
