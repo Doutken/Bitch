a05c48483de8b04ad861a135ba1315eea39af55d
Diego Rivas <diegorivas2000@gmail.com>
2021-09-27 17:27:24 -0400

    Create initial console timer

--- app.js
+++ app.js
@@ -1,14 +1,45 @@
 console.log("Hello World");
 let counter = 5000;
 function startTimer() {
-    setTimeout(() => {
-    alert("You're done!")
-    }, 5000);
-};
+  setTimeout(() => {
+    alert("You're done!");
+  }, 20000);
+}
 
-function backwardsCount() {
-    setInterval(() => {
-counter-- 
-console.log(counter)
-}, 1000)
-};
+function backwardsCount(seconds) {
+  const intervalID = setInterval(() => {
+    seconds--;
+    console.log(seconds);
+    if (seconds <= 0) stopInterval();
+  }, 1000);
+
+  function stopInterval() {
+    clearInterval(intervalID);
+  }
+}
+
+/**
+ * Funcion que consuma 2 parametros
+ * @param minutes
+ * @param seconds
+ * Hará una cuenta regresiva hasta que 'minutes' y 'seconds' sea 0
+ */
+function timer(min, sec) {
+  console.log(min, sec);
+  if (sec == 0) {
+    min--;
+    sec += 59;
+  }
+  const intervalID = setInterval(() => {
+    sec--;
+    console.log(min, sec);
+    if (sec <= 0) {
+      stopInterval();
+      if(min > 0) timer(min, sec)
+    }
+  }, 1000);
+
+  function stopInterval() {
+    clearInterval(intervalID);
+  }
+}
